0d03c5fbf9fe92bd459457f10ddc34555f9ae2fe
gabchak <andrey.gabchak@gmail.com>
2018-12-06 20:00:13 +0200

    Added '**' to all pages

--- src/main/java/com/gabchak/springbootdemo/configuration/SecurityConfiguration.java
+++ src/main/java/com/gabchak/springbootdemo/configuration/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable() //вид атаки
             .authorizeRequests()
-                .antMatchers("/").authenticated()
+                .antMatchers("/**").authenticated()
                 .antMatchers("/admin/*").hasRole("ADMIN")
                 .and()
                 .formLogin()

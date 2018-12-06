02d0aec8c6c3b60a9627055910543629f7d33e62
gabchak <andrey.gabchak@gmail.com>
2018-12-06 21:14:03 +0200

    Copied classes from previous project

---
+++ src/main/java/com/gabchak/springbootdemo/model/Role.java
@@ -0,0 +1,45 @@
+package com.gabchak.springbootdemo.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "ROLES")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "ROLE_NAME")
+    private String roleName;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "USERS_TO_ROLES",
+            joinColumns = {@JoinColumn(name = "FK_ROLE_ID")}, //{} - скобки можно убрать
+            inverseJoinColumns = {@JoinColumn(name = "FK_USER_ID")})
+    private Set<User> users = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
--- src/main/java/com/gabchak/springbootdemo/model/User.java
+++ src/main/java/com/gabchak/springbootdemo/model/User.java
@@ -1,10 +1,47 @@
 package com.gabchak.springbootdemo.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "USERS")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Column(name = "EMAIL")
     private String email;
+    @NotNull
+    @Column(name = "PASSWORD")
     private String password;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "TOKEN")
+    private String token;
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "users") //имя Set поля в Role.class
+    private Set<Role> roles = new HashSet<>();
+
+    public User(Long id, String email, String password, String firstName, String lastName, String token) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.token = token;
+    }
+
+    public User() {
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 
     public Long getId() {
         return id;
@@ -12,6 +49,11 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
     }
 
     public String getEmail() {
@@ -28,5 +70,37 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
     }
 }
